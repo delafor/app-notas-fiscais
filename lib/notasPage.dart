13a515c66eea5fe14d2d15c06076413d8e7d6f14
Delafor <edinaldooliveira003@gmail.com>
2025-10-13 23:55:22 -0300

    Adiç~ao de escolha de data, Alteracao 05

--- lib/notasPage.dart
+++ lib/notasPage.dart
@@ -1,6 +1,7 @@
 // pages/invoices_page.dart
 import 'dart:convert';
 import 'dart:io';
+import 'package:formulario/dashboardPage.dart';
 import 'package:intl/intl.dart';
 import 'package:path/path.dart' as path;
 import 'package:firebase_core/firebase_core.dart';
@@ -78,23 +79,37 @@ class _NotasPageState extends State<NotasPage> {
   XFile? nota;
 
   String _selectedDate = '';
-  String _dateCount = '';
+  // String _dateCount = '';
   String _range = '';
-  String _rangeCount = '';
+  // String _rangeCount = '';
 
   void _onSelectionChanged(DateRangePickerSelectionChangedArgs args) {
     setState(() {
-      if (args.value is PickerDateRange) {
-        _range =
-            '${DateFormat('dd/MM/yyyy').format(args.value.startDate)} - '
-            '${DateFormat('dd/MM/yyyy').format(args.value.endDate ?? args.value.startDate)}';
-      } else if (args.value is DateTime) {
-        _selectedDate = args.value.toString();
-      } else if (args.value is List<DateTime>) {
-        _dateCount = args.value.length.toString();
-      } else {
-        _rangeCount = args.value.length.toString();
+      if (args.value is DateTime) {
+        final DateTime date = args.value;
+        final formatted = DateFormat('dd/MM/yyyy').format(date);
+        _selectedDate = formatted;
+        dataController.text = formatted;
+      } else if (args.value is PickerDateRange) {
+        final start = DateFormat('dd/MM/yyyy').format(args.value.startDate);
+        final end = DateFormat(
+          'dd/MM/yyyy',
+        ).format(args.value.endDate ?? args.value.startDate);
+        _range = '$start - $end';
+        dataController.text = _range;
       }
+
+      // if (args.value is PickerDateRange) {
+      //   _range =
+      //       '${DateFormat('dd/MM/yyyy').format(args.value.startDate)} - '
+      //       '${DateFormat('dd/MM/yyyy').format(args.value.endDate ?? args.value.startDate)}';
+      // } else if (args.value is DateTime) {
+      //   _selectedDate = args.value.toString();
+      // } else if (args.value is List<DateTime>) {
+      //   _dateCount = args.value.length.toString();
+      // } else {
+      //   _rangeCount = args.value.length.toString();
+      // }
     });
   }
 
@@ -231,6 +246,7 @@ class _NotasPageState extends State<NotasPage> {
                               return null;
                             },
                           ),
+
                           // IconButton(
                           //   onPressed: () {
                           //     // TODO: implement your code here
@@ -238,19 +254,37 @@ class _NotasPageState extends State<NotasPage> {
                           //   icon: Icon(Icons.date_range),
                           // ),
                           //////////////////////////////////////////
-                          // TextFormField(
-                          //   key: Key('data'),
-                          //   controller: dataController,
-                          //   decoration: const InputDecoration(
-                          //     labelText: 'Data da Compra',
-                          //   ),
-                          //   validator: (text) {
-                          //     if (text == null || text.isEmpty) {
-                          //       return 'Campo obrigatorio';
-                          //     }
-                          //     return null;
-                          //   },
-                          // ),
+                          TextFormField(
+                            key: Key('data'),
+                            controller: dataController,
+                            decoration: const InputDecoration(
+                              labelText: 'Data da Compra',
+                            ),
+                            validator: (text) {
+                              if (text == null || text.isEmpty) {
+                                return 'Campo obrigatorio';
+                              }
+                              return null;
+                            },
+
+                            onTap: () {
+                              showDialog(
+                                context: context,
+                                builder: (context) => AlertDialog(
+                                  title: Text('Escolha uma data'),
+                                  content: Container(
+                                    height: 350,
+                                    width: 300,
+                                    child: SfDateRangePicker(
+                                      onSelectionChanged: _onSelectionChanged,
+                                      selectionMode:
+                                          DateRangePickerSelectionMode.single,
+                                    ),
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
                           const SizedBox(height: 25),
                           ElevatedButton(
                             style: ElevatedButton.styleFrom(
@@ -314,35 +348,6 @@ class _NotasPageState extends State<NotasPage> {
                             child: Text(
                               'Cadastrar Nota',
                               style: TextStyle(fontSize: 20),
-                            ),
-                          ),
-                          Positioned(
-                            left: 0,
-                            right: 0,
-                            top: 0,
-                            height: 80,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              mainAxisSize: MainAxisSize.min,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[
-                                Text('Selected date: $_selectedDate'),
-                                Text('Selected date count: $_dateCount'),
-                                Text('Selected range: $_range'),
-                                Text('Selected ranges count: $_rangeCount'),
-                              ],
-                            ),
-                          ),
-                          Positioned(
-                            child: SfDateRangePicker(
-                              onSelectionChanged: _onSelectionChanged,
-                              selectionMode: DateRangePickerSelectionMode.range,
-                              initialSelectedRange: PickerDateRange(
-                                DateTime.now().subtract(
-                                  const Duration(days: 4),
-                                ),
-                                DateTime.now().add(const Duration(days: 3)),
-                              ),
                             ),
                           ),
                         ],
